7866259db52262752ff988cdcc5fa783c57fe9d6
zharko <zharko@itstep.org>
2016-11-28 14:34:46 +0200

    marge

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,30 @@ gulp.task('sass', function(){
 		.pipe(gulp.dest('./css'));
 });
 
+gulp.task('min', function(){
+	gulp.src('libs/bootstrap-grid.min.css')
+		.pipe(uncss({
+			html: ['index.html']
+		}))
+		.pipe(cleanCSS({compatibility: 'ie8'}))
+		.pipe(gulp.dest('libs.min/'));
+	gulp.src('libs/linea/styles.css')
+		.pipe(cleanCSS({compatibility: 'ie8'}))
+		.pipe(gulp.dest('libs.min/linea/'));
+	gulp.src('libs/font-awesome/css/font-awesome.min.css')
+		.pipe(cleanCSS({compatibility: 'ie8'}))
+		.pipe(gulp.dest('libs.min/font-awesome/css/'));
+	gulp.src('libs/magnific-popup/magnific-popup.css')
+		//.pipe(uncss({
+		//	html: ['index.html']
+		//}))
+		.pipe(cleanCSS({compatibility: 'ie8'}))
+		.pipe(gulp.dest('libs.min/magnific-popup/'));
+	gulp.src('libs/animate/animate.min.css')
+		.pipe(cleanCSS({compatibility: 'ie8'}))
+		.pipe(gulp.dest('libs.min/animate/'));
+});
+
 gulp.task('sass:watch', function(){
 	gulp.watch('./scss/**/*.scss', ['sass']);
 });
